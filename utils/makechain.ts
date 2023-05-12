477ae0e47779a1e39d06662ca9bc80faef8fd73b
Jacamein <116108057+Jacamein@users.noreply.github.com>
2023-05-12 17:42:41 -0500

    Update makechain.ts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,21 +2,21 @@ import { OpenAI } from 'langchain/llms/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
-const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+const CONDENSE_PROMPT = `Dada la siguiente conversación y una pregunta de seguimiento, reformula la pregunta de seguimiento para que sea una pregunta independiente.
 
-Chat History:
+Historial del chat:
 {chat_history}
-Follow Up Input: {question}
-Standalone question:`;
+Pregunta de seguimiento: {question}
+Pregunta independiente:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+const QA_PROMPT = `Eres un asistente de inteligencia artificial útil. Utiliza las siguientes piezas de contexto para responder a la pregunta al final.
+Si no conoces la respuesta, simplemente di que no la sabes. NO trates de inventar una respuesta.
+Si la pregunta no está relacionada con el contexto, responde educadamente que estás programado para responder solo a preguntas que están relacionadas con el contexto.
 
 {context}
 
-Question: {question}
-Helpful answer in markdown:`;
+Pregunta: {question}
+Respuesta útil en markdown:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
